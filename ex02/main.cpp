868b573453cc990535cf7a6f9f98457d2e9fb767
Abdelkader <abdocooder@gmail.com>
2025-08-24 23:26:43 +0100

    ex02 done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,19 +6,30 @@
 /*   By: abenajib <abenajib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 16:54:31 by abenajib          #+#    #+#             */
-/*   Updated: 2025/08/24 23:13:15 by abenajib         ###   ########.fr       */
+/*   Updated: 2025/08/24 23:24:30 by abenajib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <iostream>
 
 int main(void)
 {
-    int N = 10;
-    Zombie* zombiehord = zombieHorde(N, "Znames");
-    for (int i = 0; i < N; i++)
-		zombiehord[i].announce();
-    std::cout << std::endl;
-    delete[] zombiehord;
-	return 0;
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << std::endl;
+	std::cout << "The memory address of the string var: " << &str << std::endl;
+	std::cout << "The value of the string var: " << str << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << stringPTR << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	std::cout << std::endl;
+
+    return 0;
 }
